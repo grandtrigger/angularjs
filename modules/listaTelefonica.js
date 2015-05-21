f38a9d7dc55fabaf2a70a9ec1f91e64d27ed9a79
ryuuka <neto.paladinus@gmail.com>
2015-05-21 16:56:15 +0000

    Insercao de funcionalidades Cadastrar Contato e Excluir Contato. Fim da Aula 3. Mais alteracoes no arquivo index.css

--- modules/listaTelefonica.js
+++ modules/listaTelefonica.js
@@ -3,13 +3,32 @@ angular.module("listaTelefonica").controller("listaTelefonicaController", functi
     $scope.app = "Lista Telefonica";
     
     $scope.contatos = [
-            {nome: "Pedro", telefone: "999-999-99"},
-            {nome: "Ana", telefone: "888-999-99"},
-            {nome: "Maria", telefone: "777-999-99"}
-        ];
+        {nome: "Pedro", telefone: "999-999-99", cor: "blue"},
+        {nome: "Ana", telefone: "888-999-99", cor: "yellow"},
+        {nome: "Maria", telefone: "777-999-99", cor: "red"}
+	];
+    $scope.operadoras = [
+    	{nome: "Oi", codigo: 14, categoria: "Celular"},
+    	{nome: "Vivo", codigo: 15, categoria: "Celular"},
+    	{nome: "Tim", codigo: 41, categoria: "Celular"},
+    	{nome: "GVT", codigo: 25, categoria: "Fixo"},
+    	{nome: "Embratel", codigo: 21, categoria: "Fixo"}
+    ];
         
     $scope.adicionarContato = function(contato) {
         $scope.contatos.push(angular.copy(contato));
         delete $scope.contato;
+    };
+    
+    $scope.apagarContato = function(contatos) {
+        $scope.contatos = contatos.filter(function(contato) {
+            if(!contato.selecionado) return contato;
+        });
+    };
+    
+    $scope.isContatoSelecionado = function(contatos) {
+        return isContatoSelecionado = contatos.some(function(contato) {
+           return contato.selecionado; 
+        });
     };
 });
